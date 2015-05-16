2c6cf163c057235505f7c0b85c24cac427747564
Evan Dennington <evandennington@gmail.com>
2015-05-16 16:26:41 -0700

    Implemented header draft

---
+++ public/templates/_header.php
@@ -0,0 +1,22 @@
+<header>
+  <div class="header-container" id="menu_container">
+
+  <div class="container">
+
+    <img class="avatar" src="http://fullyfeline.com/wp-content/uploads/2015/01/kitten-lying-400x400.jpg">
+  
+    <nav class="main-nav nav">
+      <ul id="menu">
+        <li>
+          <input style="width:500px;" placeholder="Search for a teacher or school" type="email" id="email" name="email" required>
+        </li>        
+      </ul>
+    </nav>
+
+    <div class="right social">
+      <a class="btn--secondary" href="/take-action">Log In</a>
+      <a class="btn--primary" href="/take-action">Sign Up</a>
+    </div>
+
+  </div>
+</header>
