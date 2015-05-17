ad3db962747107af477ee8edc9d75d5b3f053ee1
Evan Dennington <evandennington@gmail.com>
2015-05-17 12:40:24 -0700

    Big update to teachers page

---
+++ public/teacher.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="dist/css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,700,900,400italic' rel='stylesheet' type='text/css'>
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+
+    <?php include('./templates/_header.php'); ?>
+
+    <body>
+
+    <div class="page-wrap">
+
+      <div class="container">
+
+        <div class="sidebar">
+  
+        <!-- TEACHER AVATAR -->
+        <div class="d--b clear center">
+            <img class="avatar-xl center" src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg">
+        </div>
+
+        <!-- NAME -->
+        <div class="d--b center m--b">
+            <h2>Evan Dennington</h2>
+        </div>
+
+        <!-- TEACHER SCORE -->
+        <div class="col-3">
+          <span style="font-weight:800;letter-spacing:1px;">SCORE</span>
+          <div class="score--main green">5</div>
+        </div>
+
+        <div class="col-9 m--b">
+
+          <span class="title">Child Happiness</span>
+          <div class="progress-bar blue stripes">
+              <span class="yellow" style="width: 60%"></span>
+          </div>
+
+          <span class="title">Child Engament</span>
+          <div class="progress-bar blue stripes">
+              <span class="green" style="width: 100%"></span>
+          </div>
+
+          <span class="title">Parent Satisfaction</span>
+          <div class="progress-bar blue stripes">
+              <span class="green" style="width: 90%"></span>
+          </div>
+        </div>
+
+
+        <!-- TEACHER META INFORAMTION -->
+        <div class="col-12">
+
+            <div class="teacher__meta-grouping">
+            <h4>Location</h4>
+            <div>Yuba City Unified School District</div>
+            <div>850 Clark Ave, Yuba City, CA 95991</div>
+            </div>
+
+            <div class="teacher__meta-grouping">
+              <div class="heading">Subject</div>
+              <span>Math</span>
+            </div>
+
+            <div class="teacher__meta-grouping">           
+              <div class="heading">Grade</div>
+              <span>6th</span>
+            </div>
+            <div class="teacher__meta-grouping">
+              <div class="heading">Teacher Bio</div>
+              <p>Cred photo booth pickled, Pitchfork mumblecore banjo Godard 3 wolf moon flannel Vice umami cray Portland tote bag. Actually try-hard 90's, Tumblr hashtag kogi banjo.</p>
+            </div>
+        </div>
+
+      </div>
+
+      <!-- MAIN CONTENT AREA-->
+
+      <div class="main-content pad-width">
+
+        <!-- SUMBIT A REVIEW BUTTON -->
+        <div class="center">
+          <a href="#" class="btn--secondary x-large m--b" style="margin-top:15px;">Submit a Review</a>
+        </div>
+
+        <h3 class="parent-reviews">Parent Reviews</h3>
+
+
+         <!--  THIS IS A COMMENT BOX -->
+          <div class="comment cf">
+     
+            <div class="comment__quote">Cred photo booth pickled, Pitchfork mumblecore banjo Godard 3 wolf moon flannel Vice umami cray Portland tote bag. Actually try-hard 90's, Tumblr hashtag kogi banjo.</div>
+
+            <div class="comment_rating-collection">
+
+              <div class="review-rating">
+                <div class="score--small red">1</div><span>Student Learning</span>
+              </div>
+
+              <div class="review-rating">
+                <div class="score--small green">5</div><span>Student Engagement</span> 
+              </div>
+
+              <div class="review-rating">
+                <div class="score--small yellow">3</div><span>Student Satisfaction</span>
+              </div>
+
+            </div>
+          
+          </div>
+
+         <!--  THIS IS A COMMENT BOX -->
+
+          <div class="comment cf">
+     
+            <div class="comment__quote">Cred photo booth pickled, Pitchfork mumblecore banjo Godard 3 wolf moon flannel Vice umami cray Portland tote bag. Actually try-hard 90's, Tumblr hashtag kogi banjo.</div>
+
+            <div class="comment_rating-collection">
+
+              <div class="review-rating">
+                <div class="score--small red">1</div><span>Child Learning</span>
+              </div>
+
+              <div class="review-rating">
+                <div class="score--small green">5</div><span>Child Engagement</span> 
+              </div>
+
+              <div class="review-rating">
+                <div class="score--small yellow">3</div><span>Parent Satisfaction</span>
+              </div>
+
+            </div>
+          
+          </div>
+
+         <!--  THIS IS A COMMENT BOX -->
+
+          <div class="comment cf">
+     
+            <div class="comment__quote">Cred photo booth pickled, Pitchfork mumblecore banjo Godard 3 wolf moon flannel Vice umami cray Portland tote bag. Actually try-hard 90's, Tumblr hashtag kogi banjo.</div>
+
+            <div class="comment_rating-collection">
+
+              <div class="review-rating">
+                <div class="score--small red">1</div><span>Child Learning</span>
+              </div>
+
+              <div class="review-rating">
+                <div class="score--small green">5</div><span>Child Engagement</span> 
+              </div>
+
+              <div class="review-rating">
+                <div class="score--small yellow">3</div><span>Parent Satisfaction</span>
+              </div>
+
+            </div>
+          
+          </div>
+
+
+
+
+        </div>
+   
+
+      </div>
+   
+
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        </script>
+    </body>
+</html>
