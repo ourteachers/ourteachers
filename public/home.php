2e151b08ca5d6299db6796d0cb50b850764174d1
Evan Dennington <evandennington@gmail.com>
2015-05-17 09:46:05 -0700

    Generated Homepage Draft

---
+++ public/home.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="dist/css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,700,900,400italic' rel='stylesheet' type='text/css'>
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+
+    <body>
+    <header>
+
+
+      <div class="header-transparent" id="menu_container">
+        <div class="container">
+
+        <!-- <img class="avatar" src="http://fullyfeline.com/wp-content/uploads/2015/01/kitten-lying-400x400.jpg"> -->
+        <span class="site-logo" style="font-size: 24px;color:#FFF;"><span style="color:#B59BEC;font-weight:800;">our </span>teachers</span>
+
+        <div class="right social">
+          <a class="btn--transparent" href="/take-action">Log In</a>
+          <a class="btn--primary" href="/take-action">Sign Up</a>
+        </div>
+    </header>
+
+
+    <div class="hero--xlg">
+      <div class="hero__container overlay">
+        <div class="hero__image" style="background-image: url(./images/classroom.jpg);">
+          <div class="hero__text--center">
+            <h1 class="hero__title">Teacher ratings by parents for parents.</h1>
+            <input style="width:500px;" placeholder="Search for a teacher or school" id="main_search" name="main_search" required>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="page-wrap">
+
+        <div class="container">
+            
+        
+        </div>
+    </div>
+
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        </script>
+    </body>
+</html>
